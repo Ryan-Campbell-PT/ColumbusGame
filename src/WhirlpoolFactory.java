2b941c7c9b71c5a963a6dd6f7bc9a5fb366d6d6d
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2018-03-05 18:10:31 -0600

    Whirlpools fully working

--- src/WhirlpoolFactory.java
+++ src/WhirlpoolFactory.java
@@ -11,18 +11,16 @@ import java.util.*;
  */
 public class WhirlpoolFactory implements Observer
 {
-    LinkedList<Whirlpool> list; //this will record all created whirlpools that arent on the screen
+    private LinkedList<Whirlpool> list; //this will record all created whirlpools that arent on the screen
     private int randomNum, iter;
 
     //I want to change these in the future. Will do later
-    private Point shipLocation;
     private Ship ship;
 
     WhirlpoolFactory(Ship ship)
     {
         list = new LinkedList<>();
-        this.shipLocation = ship.getLocation();
-        randomNum = new Random().nextInt(30);
+        randomNum = new Random().nextInt(15);
         ship.addObserver(this);
         this.ship = ship;
     }
@@ -30,6 +28,7 @@ public class WhirlpoolFactory implements Observer
     @Override
     public void update(Observable o, Object arg)
     {
+        System.out.println("Observer size: " + ship.countObservers() + "\nList size: " + list.size());
         if(o instanceof Ship)
         {
             if(iter < randomNum)
@@ -51,7 +50,7 @@ public class WhirlpoolFactory implements Observer
                         pool.create();
                     }
 
-                    randomNum = new Random().nextInt(60);
+                    randomNum = new Random().nextInt(15);
                     iter = 0;
                 }
             }
@@ -62,6 +61,8 @@ public class WhirlpoolFactory implements Observer
      * Whirlpool class is a private class that is what the user will be seeing on the grid
      *
      * Whirlpools have their own variables that decide how long the whirlpools are existing on the grid
+     * Once iter becomes == to randomNum (a variable created on the whirlpools creation), the whirlpool
+     * will then remove itself from the scene and add itself to the Whirlpool.list
      */
     private class Whirlpool implements Observer
     {
@@ -80,20 +81,21 @@ public class WhirlpoolFactory implements Observer
             if(o instanceof Ship)
             {
                 if(iter < randomNum)
-                    iter++;
+                    iter++; //as long as the whirlpool should still be around
 
                 else
-                    remove();
+                    remove(); //if its time is up, remove it from the map
             }
         }
 
         private void create()
         {
+            //create two random locations on the map
             Random rand = new Random();
             int x = rand.nextInt(Explorer.getDimensions());
             int y = rand.nextInt(Explorer.getDimensions());
 
-            while(Map.getInstance().checkLocation(x, y) != 0)
+            while(Map.getInstance().checkLocation(x, y) != 0) //to make sure its always a place to be put on
             {
                 x = rand.nextInt(Explorer.getDimensions());
                 y = rand.nextInt(Explorer.getDimensions());
@@ -110,29 +112,34 @@ public class WhirlpoolFactory implements Observer
             }
             location2 = new Point(x, y);
 
-            iter = 0;
-            randomNum = rand.nextInt(30);
-            ship.addObserver(this);
-            //list.add(this);
+            iter = 0; //reset the conter
+            randomNum = rand.nextInt(30); //decide the lifespan of the whirlpool
+            ship.addObserver(this); //add the following to it
 
             loadImage1();
             loadImage2();
         }
 
+        //whenever the whirlpool is removed from the scene, its imageview and observering status is removed,
+        //and the actual whirlpool is added to the list of inactive whirlpools
         private void remove()
         {
+            //remove the images from the screen
             Explorer.getAp().getChildren().remove(imageView1);
             Explorer.getAp().getChildren().remove(imageView2);
 
             list.add(this); //add it back to the factory for use later
-            ship.deleteObserver(this);
+            ship.deleteObserver(this); //stop following the ship observing
         }
 
         //definitely can turn these into one
         private void loadImage1()
         {
-            Image image = new Image(new File("images\\whirlpool.jpg").toURI().toString(), 50, 50, true, true);
-            imageView1 = new ImageView(image);
+            if(imageView1 == null) //if its already created, no need to keep creating more
+            {
+                Image image = new Image(new File("images\\whirlpool.jpg").toURI().toString(), 50, 50, true, true);
+                imageView1 = new ImageView(image);
+            }
             imageView1.setX(location1.x * Explorer.getScaleFactor());
             imageView1.setY(location1.y * Explorer.getScaleFactor());
             Explorer.getAp().getChildren().add(imageView1);
@@ -140,8 +147,11 @@ public class WhirlpoolFactory implements Observer
 
         private void loadImage2()
         {
-            Image image = new Image(new File("images\\whirlpool.jpg").toURI().toString(), 50, 50, true, true);
-            imageView2 = new ImageView(image);
+            if (imageView2 == null)
+            {
+                Image image = new Image(new File("images\\whirlpool.jpg").toURI().toString(), 50, 50, true, true);
+                imageView2 = new ImageView(image);
+            }
             imageView2.setX(location2.x * Explorer.getScaleFactor());
             imageView2.setY(location2.y * Explorer.getScaleFactor());
             Explorer.getAp().getChildren().add(imageView2);
