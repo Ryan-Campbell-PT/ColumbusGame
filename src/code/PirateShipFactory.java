d4539a4b1d2e3c418ef575dcb87cdc7323a7c49b
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2018-03-10 00:51:47 -0600

    PirateShipFactory created and implemented. Currently unused

--- src/code/PirateShipFactory.java
+++ src/code/PirateShipFactory.java
@@ -2,6 +2,11 @@ package code;
 
 import java.util.ArrayList;
 
+/**
+ * The current set up is the way I want the factory to work
+ * I want it to be done in a way that only ships implemented through the factory
+ * will be rendered on the screen. This will all be done through the pirateList
+ */
 public class PirateShipFactory
 {
     private ArrayList<IPirateShip> pirateList;
@@ -23,6 +28,9 @@ public class PirateShipFactory
     public void addPirateShip(IPirateShip pirateShip)
     {
         this.pirateList.add(pirateShip);
+        Ship.getInstance().addObserver(pirateShip);
     }
 
+
+    public ArrayList<IPirateShip> getPirateList() { return pirateList; }
 }
