ab1fc6b91cc36dd143fe2ff8ff0eac01ddaf3c68
Ryan-Campbell-PT <ryacampbell15@gmail.com>
2018-03-16 14:20:54 -0500

    basic structure down for eels. Still working on bugs

--- src/code/FollowPirateShip.java
+++ src/code/FollowPirateShip.java
@@ -70,7 +70,7 @@ public class FollowPirateShip extends IPirateShip
                 else if(Map.getInstance().checkLocation(this.getCurrentLocation().x + 1, this.getCurrentLocation().y) == 5) //ship is here
                 {
                     this.goEast();
-                    Explorer.showLose();
+                    //Explorer.showLose();
                 }
 
             if(ship.getCurrentLocation().x < getCurrentLocation().x)
@@ -80,7 +80,7 @@ public class FollowPirateShip extends IPirateShip
                 else if(Map.getInstance().checkLocation(this.getCurrentLocation().x - 1, this.getCurrentLocation().y) == 5) //ship is here
                 {
                     this.goWest();
-                    Explorer.showLose();
+                    //Explorer.showLose();
                 }
 
             if(ship.getCurrentLocation().y < getCurrentLocation().y)
@@ -92,7 +92,7 @@ public class FollowPirateShip extends IPirateShip
                 else if(Map.getInstance().checkLocation(this.getCurrentLocation().x, this.getCurrentLocation().y - 1) == 5) //ship is here
                 {
                     this.goNorth();
-                    Explorer.showLose();
+                    //Explorer.showLose();
                 }
 
             if(ship.getCurrentLocation().y > getCurrentLocation().y)
@@ -104,7 +104,7 @@ public class FollowPirateShip extends IPirateShip
                 else if(Map.getInstance().checkLocation(this.getCurrentLocation().x, this.getCurrentLocation().y + 1) == 5) //ship is here
                 {
                     this.goSouth();
-                    Explorer.showLose();
+                    //Explorer.showLose();
                 }
         }
     }
